5fb44808d59975a965ebfe9d2cd36b7411a6b03d
jatinverma12 <jatinverma@evolvfit.in>
2022-03-10 20:55:23 +0530

    done

--- src/Row.js
+++ src/Row.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import axios from './axios'
 import './Row.css'
+import YouTube from 'react-youtube'
+import movieTrailer from 'movie-trailer'
 const Row = ({ title, fetchUrl, isLargeRow }) => {
   const [movies, setMovies] = useState([])
+  const [trailerUrl, setTrailerUrl] = useState('')
+  const [error, setError] = useState('')
 
   useEffect(() => {
     async function fetchData() {
@@ -14,6 +18,31 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
   }, [fetchUrl])
   //fetchUrl should be the dependency as if suppose different url is passes then useEffect won;t run unless fetchUrl is
   //defined as a dependency
+  const opts = {
+    height: '390',
+    width: '99%',
+    playerVars: {
+      autoplay: 0,
+    },
+  }
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl('')
+    } else {
+      movieTrailer(null, { tmdbId: movie?.id })
+        .then((url) => {
+          setError('')
+          const urlParams = new URLSearchParams(new URL(url).search)
+          setTrailerUrl(urlParams.get('v'))
+        })
+        .catch((error) => {
+          setError(
+            "Could not find this movie's trailer. Please try different movie."
+          )
+        })
+    }
+  }
 
   const base_url = 'https://image.tmdb.org/t/p/original'
   return (
@@ -23,6 +52,7 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row_poster ${isLargeRow && 'row_posterLarge'} `}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -30,6 +60,10 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
             alt={movie.name}
           />
         ))}
+      </div>
+      <div style={{ padding: '40px' }}>
+        {error && <h2 style={{ color: 'red' }}>{error}</h2>}
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
       </div>
     </div>
   )
